132f8867c762d155882fab35ad843d3928b32fe5
Rebekah Webb <bekahwebb@gmail.com>
2024-09-13 09:55:40 -0600

    Poisson Model

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -5,7 +5,9 @@ library(tidyverse)
 library(patchwork)
 library(readr)
 library(GGally)
+library(poissonreg)
 
+#September 9 2024
 
 bike_test <- vroom("C:/Users/bekah/Downloads/bike-sharing-demand/test.csv")
 bike_test
@@ -22,7 +24,7 @@ variable_plot <- DataExplorer::plot_intro(bike_train) # visualization of glimpse
 correlation_plot <- DataExplorer::plot_correlation(bike_train) # correlation heat map 
 
 
-#scatterplots
+#plots
 plot1 <- ggplot(bike_train, aes(x = weather)) +
   geom_bar() +
   labs(title = "Frequency of Categories", x = "Weather", y = "Count") +
@@ -37,16 +39,19 @@ plot4 <- ggplot(bike_train, aes(x = windspeed, y = count)) +
   geom_point() +
   geom_smooth(se=FALSE)
 
+plot1
+
+
 plot1 + plot2 #side by side
 plot3 / plot4 #stacked
 four_panel <- (plot1 + plot2) / (plot3 + plot4) #4 panel plot
 four_panel
 
-
-## Setup and Fit the Linear Regression Model3
-my_linear_model <- linear_reg() %>% #Type of model4
-  set_engine("lm") %>% # Engine = What R function to use5
-  set_mode("regression") %>% # Regression just means quantitative response6
+#September 11, 2024
+## Setup and Fit the Linear Regression Model
+my_linear_model <- linear_reg() %>% #Type of model
+  set_engine("lm") %>% # Engine = What R function to use
+  set_mode("regression") %>% # Regression just means quantitative response
   fit(formula=count~weather+temp+humidity+windspeed, data=bike_train)
 my_linear_model
 
@@ -55,7 +60,7 @@ bike_predictions <- predict(my_linear_model,
                             new_data=bike_test) # Use fit to predict11
 bike_predictions ## Look at the output
 
-## Format the Predictions for Submission to Kaggle1
+## Format the Predictions for Submission to Kaggle
 kaggle_submission <- bike_predictions %>%
   bind_cols(., bike_test) %>% #Bind predictions with test data
   select(datetime, .pred) %>% #Just keep datetime and prediction variables
@@ -63,5 +68,29 @@ kaggle_submission <- bike_predictions %>%
   mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 
-## Write out the file9
+## Write out the file
 vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+
+#September 13 2024
+
+#Kaggle submission with poisson model
+my_pois_model <- poisson_reg() %>% #Type of model
+  set_engine("glm") %>% # GLM = generalized(not normal) linear model-cause of DF Null dev residual dev BIC output?
+  set_mode("regression") %>%
+fit(formula=count~weather+temp+humidity+windspeed , data=bike_train)
+my_pois_model
+
+## Generate Predictions Using Linear Model
+bike_predictions <- predict(my_pois_model,
+                            new_data=bike_test) # Use fit to predict
+bike_predictions ## Look at the output
+
+## Format the Predictions for Submission to Kaggle
+pois_kaggle_submission <- bike_predictions %>%
+bind_cols(., bike_test) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
+
+## Write out the file8
+vroom_write(x=pois_kaggle_submission, file="./PoissonPreds.csv", delim=",")
