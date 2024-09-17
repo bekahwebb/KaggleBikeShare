20f438baf8c93c0e9db73a00185ed604eb587026
Rebekah Webb <bekahwebb@gmail.com>
2024-09-17 13:53:41 -0600

    better kaggle score

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -6,6 +6,11 @@ library(patchwork)
 library(readr)
 library(GGally)
 library(poissonreg)
+library(recipes)
+library(rsample)
+library(magrittr)
+library(tidymodels)
+library(lubridate)
 
 #September 9 2024
 
@@ -17,7 +22,7 @@ bike_sample <- vroom("C:/Users/bekah/Downloads/bike-sharing-demand/sampleSubmiss
 bike_sample
 
 #EDA on training data
-ggpairs(bike_train)# not loading plots for some reason, maybe too big
+#ggpairs(bike_train, eval = "false")# not loading plots for some reason, maybe too big
 
 variable_plot <- DataExplorer::plot_intro(bike_train) # visualization of glimpse! 
 
@@ -52,7 +57,7 @@ four_panel
 my_linear_model <- linear_reg() %>% #Type of model
   set_engine("lm") %>% # Engine = What R function to use
   set_mode("regression") %>% # Regression just means quantitative response
-  fit(formula=count~weather+temp+humidity+windspeed, data=bike_train)
+  fit(formula=log(count)~weather+temp+humidity+windspeed, data=bike_train)
 my_linear_model
 
 ## Generate Predictions Using Linear Model
@@ -66,6 +71,7 @@ kaggle_submission <- bike_predictions %>%
   select(datetime, .pred) %>% #Just keep datetime and prediction variables
   rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
   mutate(count=pmax(0, count)) %>% #pointwise max of (0, prediction)
+  mutate(count = exp(count))%>%
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 
 ## Write out the file
@@ -92,15 +98,16 @@ bind_cols(., bike_test) %>% #Bind predictions with test data
   rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
   mutate(datetime=as.character(format(datetime))) #needed for right format to Kaggle
 
-## Write out the file8
+## Write out the file
 vroom_write(x=pois_kaggle_submission, file="./PoissonPreds.csv", delim=",")
 
-#Data wrangling September 16, 2024
+#Data wrangling September 16, 2024 and making recipe and workflow functions
 #1 clean data
-# Data Cleaning
+# Data Cleaning may be overfitting the data, warning for the response, keep adjusting the features so I don't 
+#have too much correlated data.
 
 clean_bike <- bike_train %>%
-  select(-c('casual', 'registered')) %>%
+  select(-c('casual','registered')) %>%
   mutate(count = log(count))
 
 
@@ -108,20 +115,26 @@ clean_bike <- bike_train %>%
 
 my_recipe <- recipe(count ~ ., data = clean_bike) %>%
   step_mutate(weather = ifelse(weather== 4,3, weather)) %>%  # Recode weather category
-  step_mutate(weather = factor(weather)) %>% #Convert season to factor
-  step_mutate(season = factor(season)) %>%  # Convert season to factor
-  step_time(datetime, features = "hour") %>%  # Extract hour from datetime column
+  step_mutate(weather = factor(weather, levels=1:4, labels=c("Clear", "Mist", "Snow", "Rain"))) %>% #Convert season to factor
+  # step_mutate(season = factor(season, levels=1:4, labels=c("Spring","Summer","Fall", "Winter"))) %>%  # Convert season to factor
+  # step_mutate(holiday = factor(holiday, levels=c(0,1), labels=c("No", "Yes")))%>% # Convert holiday as a factor
+  # step_mutate(workingday = factor(workingday, levels=c(0,1), labels=c("No", "Yes")))%>% #Convert workingday as a factor
+  # step_mutate(minute = as.factor(minute(datetime)))%>%  # Extract min. from datetime column
+  step_mutate(hour = as.factor(hour(datetime))) %>%
+  # step_mutate(day = as.factor(wday(datetime))) %>%
+  # step_mutate(month = as.factor(month(datetime))) %>%
   step_rm(datetime) %>%
   step_zv(all_predictors()) %>%  # Remove zero-variance predictors
   step_dummy(all_nominal_predictors()) %>%  # Create dummy variables
-  step_poly(temp, degree = 2) %>%  # Create polynomial features for temp
-  step_corr(all_predictors(), threshold = 0.5)  # Remove highly correlated predictors
+  step_poly(temp, degree = 2)%>%  # Create polynomial features for temp
+  #step_poly(windspeed, degree=2) %>%
+  step_corr(all_predictors())  # Remove highly correlated predictors
 
 # Preprocessing
 prepped_recipe <- prep(my_recipe)
 #Baking
-bake(prepped_recipe, new_data = bike_train)
-
+test <- bake(prepped_recipe, new_data = bike_train)
+test
 
 ## Define a Model
 lin_model <- linear_reg() %>%
@@ -131,18 +144,20 @@ lin_model <- linear_reg() %>%
 bike_workflow <- workflow() %>%
   add_recipe(my_recipe) %>%
   add_model(lin_model) %>%
-  fit(data=bike_train)
+  fit(data=bike_train)#fit the workflow
 
 ## Run all the steps on test data
 lin_preds <- predict(bike_workflow, new_data = bike_test)
 
 ## Format the Predictions for Submission to Kaggle
 linear_kaggle_submission <- lin_preds %>%
+  mutate(count = exp(.pred)) %>%  # Back-transform the log to original scale
   bind_cols(., bike_test) %>% #Bind predictions with test data
   select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  mutate(datetime=as.character(format(datetime))) %>% #needed for right format to Kaggle
   rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
-  mutate(datetime=as.character(format(datetime)))#%>% #needed for right format to Kaggle
-#mutate(count = exp(count))
+  mutate(count=pmax(0, count))
+  
 
 ## Write out the file
 vroom_write(x=linear_kaggle_submission, file="BakedlinearPreds.csv", delim=",")
