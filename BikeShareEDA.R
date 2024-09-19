b351e557aba301e8403493e740ceb50bd8b776ce
Rebekah Webb <bekahwebb@gmail.com>
2024-09-18 23:31:38 -0600

    Penalized Preds

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -11,6 +11,8 @@ library(rsample)
 library(magrittr)
 library(tidymodels)
 library(lubridate)
+library(poissonreg) #if you want to do penalized, poisson regression
+
 
 #September 9 2024
 
@@ -111,22 +113,26 @@ clean_bike <- bike_train %>%
   mutate(count = log(count))
 
 
-# Feature Engineering
+
+# Feature Engineering #maybe create an interaction between hour and weekday, maybe make a hour a factor
 
 my_recipe <- recipe(count ~ ., data = clean_bike) %>%
   step_mutate(weather = ifelse(weather== 4,3, weather)) %>%  # Recode weather category
-  step_mutate(weather = factor(weather, levels=1:4, labels=c("Clear", "Mist", "Snow", "Rain"))) %>% #Convert season to factor
-  # step_mutate(season = factor(season, levels=1:4, labels=c("Spring","Summer","Fall", "Winter"))) %>%  # Convert season to factor
+  step_mutate(weather = factor(weather, levels=1:3, labels=c("Clear", "Mist", "Precipitation"))) %>% #Convert season to factor
+  step_mutate(season = factor(season, levels=1:4, labels=c("Spring","Summer","Fall", "Winter"))) %>%  # Convert season to factor
   # step_mutate(holiday = factor(holiday, levels=c(0,1), labels=c("No", "Yes")))%>% # Convert holiday as a factor
   # step_mutate(workingday = factor(workingday, levels=c(0,1), labels=c("No", "Yes")))%>% #Convert workingday as a factor
   # step_mutate(minute = as.factor(minute(datetime)))%>%  # Extract min. from datetime column
-  step_mutate(hour = as.factor(hour(datetime))) %>%
-  # step_mutate(day = as.factor(wday(datetime))) %>%
+  step_time(datetime, features = "hour")%>% #extract hour
+  step_mutate(datetime_hour = factor(datetime_hour, levels=c(0:23), labels=c(0:23))) %>% #factor hour
+  step_date(datetime, features = "dow" )%>% #extract day of week
+  #step_mutate(workingday = factor(workingday, levels=c(0,1), labels=c("No", "Yes")))%>% #Convert workingday as a factor
   # step_mutate(month = as.factor(month(datetime))) %>%
+  #step_interact(terms = ~ hour*dow)%>% #create interaction term between hour and day of the week
   step_rm(datetime) %>%
   step_zv(all_predictors()) %>%  # Remove zero-variance predictors
   step_dummy(all_nominal_predictors()) %>%  # Create dummy variables
-  step_poly(temp, degree = 2)%>%  # Create polynomial features for temp
+  #step_poly(temp, degree = 2)%>%  # Create polynomial features for temp
   #step_poly(windspeed, degree=2) %>%
   step_corr(all_predictors())  # Remove highly correlated predictors
 
@@ -161,3 +167,47 @@ linear_kaggle_submission <- lin_preds %>%
 
 ## Write out the file
 vroom_write(x=linear_kaggle_submission, file="BakedlinearPreds.csv", delim=",")
+
+#9/18/24 Penalized Regression
+
+## Create a recipe
+bike_recipe <- recipe(count~., data=clean_bike) %>%
+  step_time(datetime, features="hour") %>% #extract hour
+  step_mutate(datetime_hour= factor(datetime_hour, levels=c(0:23), labels=c(0:23))) %>%
+  step_date(datetime, features = "dow" )%>% #extract day of week
+  step_interact(terms = ~ datetime_hour*workingday)%>%
+  step_rm(datetime) %>% 
+  step_dummy(all_nominal_predictors()) %>% #make dummy variables
+  step_normalize(all_numeric_predictors())%>% # Make mean 0, sd=1
+  step_corr(all_predictors())  # Remove highly correlated predictors
+
+
+# Preprocessing
+prepped_recipe <- prep(bike_recipe)
+#Baking
+test1 <- bake(prepped_recipe, new_data = bike_train)
+#test1
+
+## Penalized regression model
+preg_model <- linear_reg(penalty=0, mixture= 0) %>% #Set model and tuning so far 0,0 got me my lowest score
+  set_engine("glmnet") # Function to fit in R
+preg_wf <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(preg_model) %>%
+  fit(data=bike_train)
+
+## Run all the steps on test data
+predict(preg_wf, new_data=bike_test)
+
+## Format the Predictions for Submission to Kaggle
+preg_linear_kaggle_submission <- predict(preg_wf, new_data=bike_test) %>%
+  mutate(count = exp(.pred)) %>%  # Back-transform the log to original scale
+  bind_cols(., bike_test) %>% #Bind predictions with test data
+  select(datetime, .pred) %>% #Just keep datetime and prediction variables
+  mutate(datetime=as.character(format(datetime))) %>% #needed for right format to Kaggle
+  rename(count=.pred) %>% #rename pred to count (for submission to Kaggle)
+  mutate(count=pmax(0, count))
+
+
+## Write out the file
+vroom_write(x=preg_linear_kaggle_submission, file="PenalizedPreds.csv", delim=",")
